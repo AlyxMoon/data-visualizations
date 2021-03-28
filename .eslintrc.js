2ce353e3cf712953f8e81767c1496174cac43ddb
AlyxMoon <allisterkmoon@gmail.com>
2021-03-28 12:13:50 -0700

    update eslint so .ts files are parsed correctly

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     node: true,
   },
   extends: [
+    'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
     '@vue/standard',
     '@vue/typescript/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
